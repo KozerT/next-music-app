2652773c94af499bb3a7caa447842c8b701e387e
KozerT <kozer.vizastaff@gmail.com>
2024-05-15 23:02:32 +0200

    feat: add a Header component, that have navigation for both desktop and mobile versions

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,14 +14,15 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <SideBar>{children}</SideBar>
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <SideBar>{children}</SideBar>
+        </body>
     </html>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,47 @@
+"use client"
+import { useRouter } from "next/navigation";
+import { ReactNode } from "react"
+import { BiSearch } from "react-icons/bi";
+import { HiHome } from "react-icons/hi";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { twMerge } from "tailwind-merge";
+
+type HeaderProps = {
+    children: React.ReactNode;
+    className?: string
+}
+
+const Header: React.FC<HeaderProps> = ({children,className }) => {
+    const router = useRouter();
+    const handleLogOut = () => {
+        //handle functionality
+    }
+  return (
+    <>
+    <header className={twMerge(`h-fit bg-gradient-to-b from-emerald-900 p-6`, className)}>
+     <div className=" w-full mb-4 flex items-center justify-between">
+        <div className="hidden md:flex gap-x-2 items-center">
+            <button className="bg-black rounded-full flex items-center hover:opacity-75 transition " onClick={()=> router.back()}>
+                <RxCaretLeft size={35} className=" text-wite"/>
+            </button>
+            <button className="bg-black rounded-full flex items-center hover:opacity-75 transition">
+                <RxCaretRight size={35} className=" text-wite"
+                onClick={()=> router.forward()}
+                />
+            </button>
+        </div>
+        <div className="flex  md:hidden gap-x-2 items-center">
+           <button className="rounded-full p-2 bg-slate-50 flex items-center justify-center hover:opacity-80 transition">
+            <HiHome className="text-black" size={20}/>
+             </button>
+             <button className="rounded-full p-2 bg-slate-50 flex items-center justify-center hover:opacity-80 transition">
+            <BiSearch className="text-black" size={20}/>
+             </button>
+        </div>
+     </div>
+    </header>
+    </>
+  )
+}
+
+export default Header
