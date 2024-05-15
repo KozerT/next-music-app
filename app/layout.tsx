70a3abc01a188d6efae2d67ee193609779bf1bf6
KozerT <kozer.vizastaff@gmail.com>
2024-05-15 21:58:31 +0200

    feat: intriduce a sideBar item component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import SideBar from "@/components/SideBar";
 
 const font = Poppins({
   subsets: ["latin"],
@@ -19,6 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <SideBar>{children}</SideBar>
       <body className={font.className}>{children}</body>
     </html>
   );
---
+++ types/types.tsx
@@ -0,0 +1,3 @@
+type SideBarProps = {
+  children: React.ReactNode
+}
