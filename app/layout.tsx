05edb5fe421840a99aa1d54edb52d60aee73aa7b
KozerT <kozer.vizastaff@gmail.com>
2024-05-18 23:14:34 +0200

    feat: create a UserProvider and wrap the SideBar in the layout with the provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import SideBar from "@/components/SideBar";
-import SupabaseProvider from "@/SupabaseProvider";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+
 
 const font = Poppins({
   subsets: ["latin"],
@@ -23,7 +25,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <SupabaseProvider>
+        <UserProvider>
         <SideBar>{children}</SideBar>
+        </UserProvider>
         </SupabaseProvider>
         </body>
     </html>
