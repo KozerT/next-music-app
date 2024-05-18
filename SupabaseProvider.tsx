ea5cab7f2323fa14fa4ff83926fa53277e802fce
KozerT <kozer.vizastaff@gmail.com>
2024-05-18 14:17:26 +0200

    chore: installed a dependncies for the @supabase/auth-helpers-nextjs AND @supabase/auth-helpers-react

---
+++ SupabaseProvider.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import {Database} from '@/types_db'; 
+
+type SupabaseProviderProps = {
+     children: React.ReactNode;
+}
+
+
+const SupabaseProvider: React.FC<SupabaseProviderProps> = ({children}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SupabaseProvider
