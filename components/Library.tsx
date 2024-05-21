03c2b746a8e0c6315c16c28299a37f9308c35af3
KozerT <kozer.vizastaff@gmail.com>
2024-05-21 22:47:50 +0200

    feat: setup a UploadModal comopnent

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import useAuthModal from "@/hooks/useAuthModal"
+import useUploadModal from "@/hooks/useUploadModal"
 import { useUser } from "@/hooks/useUser"
 import { AiOutlinePlus } from "react-icons/ai"
 import { TbPlaylist } from "react-icons/tb"
@@ -11,12 +12,15 @@ type Library = {
 
 const Library: React.FC<Library> = () => {
   const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
   const {user} = useUser();
 
     const onClick = () => {
        if(!user) {
         return authModal.onOpen();
        }
+        //subscr. check should be here; 
+       return uploadModal.onOpen();
     }
   return (
     <div className="flex flex-col">
