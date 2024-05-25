b17c194be58c026ad29c794a3fe28b403ee97397
KozerT <kozer.vizastaff@gmail.com>
2024-05-25 13:15:15 +0200

    feat: configure the next.config to display images from the supabase

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types"
+import Image from "next/image";
 
 type SongItemProps = {
     data: Song;
@@ -11,9 +13,20 @@ type SongItemProps = {
 const SongItem: React.FC<SongItemProps> = ({
     data, onCLick
 }) => {
+    const imagePath = useLoadImage(data);
+
   return (
-    <div>
-      Song Item
+    <div onClick={() => onCLick(data.id)}
+    className="group relative flex flex-col items-center justify-center rounded-md overflow-hidden gap-x-4 p-3 bg-neutral-400/5 cursor-pointer hover:bg-neutral-400/10 transition "
+    >
+      < div className="w-full h-full  rounded-md relative aspect-square overflow-hidden">
+        <Image 
+        className="object-cover"
+        src={imagePath || '/images/like.png'}
+        fill
+        alt="Image"
+        />
+      </div>
     </div>
   )
 }
