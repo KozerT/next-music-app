907feff9ee67d80b07bc37701bfec382e31cedc0
KozerT <kozer.vizastaff@gmail.com>
2024-05-29 22:22:26 +0200

    feat: create a hook to use song url

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,12 +1,17 @@
 'use client'
 
+import useGetSongById from "@/hooks/useGetSongById";
 import usePlayer from "@/hooks/usePlayer"
 
 const Player = () => {
     const player = usePlayer();
+    const {song}=useGetSongById(player.activeId)
+
+    //client component; 
+    // load mp3 files. we can do this by reading path
   return (
     <div>
-      Player!
+      !
     </div>
   )
 }
