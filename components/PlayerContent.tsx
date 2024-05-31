6ca53aadef3e132eaa94960b963778e2676b0c4a
KozerT <kozer.vizastaff@gmail.com>
2024-05-31 13:39:37 +0200

    chore: instal a use-sound hook to have play music functionality [https://www.joshwcomeau.com/react/announcing-use-sound-react-hook/]

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -5,10 +5,12 @@ import MediaItem from "./MediaItem";
 import LikeButton from "./LikeButton";
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
-import { ImVolumeIncrease } from "react-icons/im";
-import { BiVolume } from "react-icons/bi";
+
 import { IoVolumeHighOutline } from "react-icons/io5";
 import Slider from "./Slider";
+import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
+import usePlayer from "@/hooks/usePlayer";
+import { useState } from "react";
 
 type PlayerContentProps = {
      song: Song;
@@ -19,7 +21,45 @@ type PlayerContentProps = {
 const PlayerContent: React.FC <PlayerContentProps> = ({
     song, songUrl
 }) => {
+
+  const player = usePlayer();
+  const[volume, setVolume]=useState(1);
+  const [isPlaying , setIsPlaying]=useState(false);
+
     const Icon = true ? BsPauseFill : BsPlayFill;
+    const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave;
+
+    const onPlayNext = ()=> {
+      if(player.ids.length === 0){
+        return
+      }
+
+    const currentIndex = player.ids.findIndex((id)=> id === player.activeId);
+    const nextSong = player.ids[currentIndex +1];
+
+    if(!nextSong){
+      return player.setId(player.ids[0]);
+    }
+
+    player.setId(nextSong)
+  }
+
+
+  const onPlayPrevious = ()=> {
+    if(player.ids.length === 0){
+      return
+    }
+
+  const currentIndex = player.ids.findIndex((id)=> id === player.activeId);
+  const prevSong = player.ids[currentIndex -1];
+
+  if(!prevSong){
+    return player.setId(player.ids[player.ids.length -1]);
+  }
+
+  player.setId(prevSong)
+}
+
   return (
 
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
@@ -38,21 +78,22 @@ const PlayerContent: React.FC <PlayerContentProps> = ({
      </div>
      <div className="hidden h-full md:flex justify-center items-center w-full max-w-[772px] gap-x-6">
       <AiFillStepBackward size={30} className="text-white transition text-neutral-400 cursor-pointer hover: text-white"/>
-      <div onClick={()=> {}}
+      <div onClick={onPlayPrevious}
       className="flex items-center justify-center w-10 h-10 rounded-full bg-white p-1 cursor-pointer"
       >
         <Icon size={30} className="text-black"/>
       </div>
       <AiFillStepForward
-      onClick={()=> {  }}
+      onClick={onPlayNext}
       size={30}
+
       className="text-neutral-400 cursor-pointer hover:text-white transition"
       />
      </div>
 
      <div className="hidden w-full md:flex w-full grid-cols-2  md:grid-cols-3 h-full ">
         <div className="flex items-center gap-x-4 w-[120px]">
-          <IoVolumeHighOutline onClick={()=>{}} className="cursor-pointer"/>
+          <VolumeIcon onClick={()=>{}} className="cursor-pointer"/>
           <Slider/>
         </div>      
      </div>
--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,10 +1,28 @@
     import React from 'react'
-    
-    const Slider = () => {
+    import * as RadixSlider from '@radix-ui/react-slider'
+
+    type sliderProps = {
+      value?: number;
+      onChange?: (value:number)=> void;
+    }
+
+    const Slider: React.FC<sliderProps> = ({value=1, onChange}) => {
+      const handleChange = (newValue: number[]) =>{
+        onChange?.(newValue[0]);
+      }
       return (
-        <div>
-          Slider
-        </div>
+      <RadixSlider.Root className='relative flex flex-center select-none touch-none w-full h-10'
+        value={[value]}
+        defaultValue={[1]}
+        onValueChange={handleChange}
+        max={1}
+        step={0.1}
+        aria-label='Volume'
+      >
+      <RadixSlider.Track className='bg-neutral-600 relative grow rounded-full h-[3px]'>
+       <RadixSlider.Range className='absolute bg-white rounded-full h-full'/>
+      </RadixSlider.Track>
+      </RadixSlider.Root>
       )
     }
     
